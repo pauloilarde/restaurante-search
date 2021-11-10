21b9053c237c05b2b9a7ec7ac35d5dade707866f
Paulo Ilarde <pauloilarde@gmail.com>
2021-11-10 19:16:49 -0300

    Commit único com tudo funcionando

---
+++ src/components/RestaurantCard/index.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import ReactStars from 'react-rating-stars-component';
+
+import Skeleton from '../Skeleton';
+
+import restaurante from '../../assets/restaurante-fake.png';
+
+import { Address, Restaurant, RestaurantInfo, Title, RestaurantPhoto } from './style';
+
+const RestaurantCard = ({ restaurant, onClick }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+  return (
+    <Restaurant onClick={onClick}>
+      <RestaurantInfo>
+        <Title>{restaurant.name}</Title>
+        <ReactStars count={5} isHalf value={restaurant.rating} edit={false} activeColor="#e7711c" />
+        <Address>{restaurant.vicinity || restaurant.formatted_address}</Address>
+      </RestaurantInfo>
+      <RestaurantPhoto
+        imageLoaded={imageLoaded}
+        src={restaurant.photos ? restaurant.photos[0].getUrl() : restaurante}
+        onLoad={() => setImageLoaded(true)}
+        alt="Foto do restaurante"
+      />
+      {!imageLoaded && <Skeleton width="100px" height="100px" />}
+    </Restaurant>
+  );
+};
+
+export default RestaurantCard;
